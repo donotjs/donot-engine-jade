f639f147a4b67fef44b619dd5724b14786a38a16
Kristian Trenskow <trenskow@me.com>
2015-06-11 00:10:54 +0200

    Added render callback for options.

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 var jade = require('jade');
 
-exports = module.exports = {
-  map: {
-    '.html': '.jade',
-    '.htm': '.jade'
-  },
-  compile: function(file, data, encoding, cb) {
-    var fn;
-    try {
-      fn = jade.compileClientWithDependenciesTracked(data, { filename: file });
-    } catch (err) {
-      return cb(err);
-    }
-    cb(null, fn.body, [file].concat(fn.dependencies));
-  },
-  render: function(url, data, encoding, cb) {
-    var source;
-    try {
-      var fn = new Function('return ' + data);
-      source = fn()();
-    } catch (err) {
-      return cb(err);
-    }
-    cb(null, source);
-  }
+exports = module.exports = function(optCb) {
+    return {
+      map: {
+        '.html': '.jade',
+        '.htm': '.jade'
+      },
+      compile: function(file, data, encoding, cb) {
+        var fn;
+        try {
+          fn = jade.compileClientWithDependenciesTracked(data, { filename: file });
+        } catch (err) {
+          return cb(err);
+        }
+        cb(null, fn.body, [file].concat(fn.dependencies));
+      },
+      render: function(url, data, encoding, cb) {
+        var source;
+        try {
+          var fn = new Function('return ' + data);
+          source = fn()(optCb ? optCb() : null);
+        } catch (err) {
+          return cb(err);
+        }
+        cb(null, source);
+      }
+    };
 };
